2851feca5ea32dfc7ce2cb0ce2b482118661b81d
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-15 11:31:29 -0300

    primeros detalles del header, con menu hamburguesa, falta pulir detalles de esta seccion

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-
-import './NavBar.css'
+import React from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <>
-        <header>
-            <nav>
-                <h1>Salustiano Robles Teran</h1>
+    <header>
+      <nav>
+        <h1 className='logo'>Salustiano Robles Teran</h1>
+        <input type="checkbox" id="menu-icon" className="menu-icon"/>
+        <label htmlFor="menu-icon" className="menu-hamburguesa">
+          <i className="bi bi-list"></i>
+        </label>
+        <ul>
+          <li><a href="#">Sobre Mi</a></li>
+          <li><a href="#">Mi Experiencia</a></li>
+          <li><a href="#">Proyectos</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-                <input type="checkbox" id="menu-toggle"/>
-                <label className="menu-icon"><i className="fas fa-bars"></i></label>
-
-                <ul>
-                    <li><a href="#">Sobre Mi</a> <i className="fas fa-bars"></i></li>
-                    <li><a href="#">Mi Experiencia</a></li>
-                    <li><a href="#">Proyectos</a></li>
-                    <li><a href="#">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>
-    </>
-  )
-}
-
-export default NavBar
+export default NavBar;
--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -9,9 +9,9 @@ const Perfil = () => {
         <img src="../src/assets/img/profile.png" alt="Salustiano Robles Teran profile picture" />
       </div>
       <div className="section__text">
-        <p className="section__text__p1">Hola! mi nombre es</p>
-        <h1 className="title">Salustiano Robles Teran</h1>
-        <p className="section__text__p2">Full Stack Developer</p>
+        <p className="section__text__p1">Mi nombre es </p>
+        <h2 className="title">Salustiano Robles Teran</h2>
+        <p className="section__text__p2">Soy estudiante de Ingeniería Informática, apasionado de la tecnología y el desarrollo de software. Recientemente completé un bootcamp de programación full stack, donde adquirí habilidades prácticas en diversas tecnologías y lenguajes de programación. Estoy en constante búsqueda de nuevos desafíos que me permitan aplicar y expandir mis conocimientos en el campo de la informática.</p>
         <div className="btn-container">
           <button className="btn btn-color-2">Download CV</button>
           <button className="btn btn-color-1">Contact Info</button>
